e27a139936b8d4c6874d7265f13082842ffc139c
unknown <daljitsingh365@yahoo.com>
2020-12-02 20:25:57 -0500

    minor changes

--- make-cookies.php
+++ make-cookies.php
@@ -4,7 +4,9 @@
 		if($_POST["theme"] !== '0' && $_POST["philosopher"] !== '0'	){
 			$theme = $_POST["theme"];
 			$philosopher = $_POST["philosopher"];
-			setcookie("theme", $theme, time() + 86400, "/");
+			//persistent cookie
+			setcookie("theme", $theme, time() + (86400 * 30), "/");
+			//session cookie
 			setcookie("philosopher", $philosopher,0,"/");
 			header("Location: chapter16-project1.php");
 		} else {
